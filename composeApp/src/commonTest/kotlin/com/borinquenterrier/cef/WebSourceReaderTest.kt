a7423bef705ef7cb8b8823083de4d98c2b32a2c7
Walter Duque de Estrada <wbduque@mac.com>
2026-06-07 08:14:08 -0500

    refactor: simplify WebSourceReaderTest and correct entity decoding expected output

--- composeApp/src/commonTest/kotlin/com/borinquenterrier/cef/WebSourceReaderTest.kt
+++ composeApp/src/commonTest/kotlin/com/borinquenterrier/cef/WebSourceReaderTest.kt
@@ -2,8 +2,8 @@ package com.borinquenterrier.cef
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldNotContain
 import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
 
 class WebSourceReaderTest : FunSpec({
 
@@ -28,6 +28,41 @@ class WebSourceReaderTest : FunSpec({
         cleanText shouldNotContain "<html>"
         cleanText shouldContain "Course Schedule"
         cleanText shouldContain "Exam on 2024-12-10"
-        // We might want to filter out 'Menu' or nav, but basic cleaning is the priority
+    }
+
+    test("should clean HTML with correct script and style removal") {
+        val html = "<html><body><script>alert('Hello World!');</script><style>body { background-color: blue; }</style></body></html>"
+        val expected = "" // Because both script and style are removed, and body has no direct text
+
+        val reader = WebSourceReader()
+        val result = reader.cleanHtml(html)
+        result shouldBe expected
+    }
+
+    test("should clean HTML with correct tag removal") {
+        val html = "<p>Hello <span>World!</span></p>"
+        val expected = "Hello World!"
+
+        val reader = WebSourceReader()
+        val result = reader.cleanHtml(html)
+        result shouldBe expected
+    }
+
+    test("should clean HTML with correct entity decoding") {
+        val html = "&nbsp;&amp;&lt;&gt;&quot;"
+        val expected = "&<>\""
+
+        val reader = WebSourceReader()
+        val result = reader.cleanHtml(html)
+        result shouldBe expected
+    }
+
+    test("should return correctly formatted text after normalization and trimming of whitespace") {
+        val html = "<html><body>   Hello World!   </body></html>"
+        val expected = "Hello World!"
+
+        val reader = WebSourceReader()
+        val result = reader.cleanHtml(html)
+        result shouldBe expected
     }
 })

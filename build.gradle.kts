ac44acdccb29374a1a1b13c4a3d83cc024818f3b
Walter Duque de Estrada <wbduque@mac.com>
2026-06-07 00:03:04 -0500

    feat(test): add JetBrains Kover code coverage plugin
    - Add Kover dependency version and plugin to libs.versions.toml version catalog
    - Declare Kover plugin in root build.gradle.kts
    - Apply Kover plugin to composeApp build.gradle.kts to enable test coverage report generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.ktor) apply false
+    alias(libs.plugins.kover) apply false
 }
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.composeHotReload)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.sqldelight)
+    alias(libs.plugins.kover)
 }
 
 val generateBuildSecrets = tasks.register("generateBuildSecrets") {
